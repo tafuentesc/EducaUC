f3d67c1ac52d656fbbac01a55edad0545ec74808
xas <afchacon2@gmail.com>
2013-05-21 20:30:33 -0400

    project reset

---
+++ educaUC/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EducaUC::Application.config.secret_token = 'b1677e95ab3b8d9956fc3d8060f7f180d21595b758952f79ba1f2f14ecfb8ccc1ecac70a0a1a6066a7c2580edccd4225286bdc4319c84f9bacce2d30641bb429'
