e8453ee3ac9a359fac326ed692c02d516de65f2e
tomas :fuentes <tafuentesc@gmail.com>
2013-05-26 17:40:36 -0400

    Se deja funcionando la creación y edición de evaluaciones. BUG1: al marcar N/A no se guarda en la BD; BUG2: no siempre los indicadores se retornan en el orden adecuado => se produce un error al dibujar el form. BUG3: uso de hidden_fields (falla de seguridad... al menos habría que encriptarlos).

--- educaUC/app/models/item.rb
+++ educaUC/app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ActiveRecord::Base
-  attr_accessible :eval, :item_template_id, :subescala_id, :observaciones
+  attr_accessible :eval, :item_template_id, :subescala_id, :observaciones, :indicador_attributes
   has_many :indicador, :dependent => :destroy
   belongs_to :subescala
   belongs_to :item_template
