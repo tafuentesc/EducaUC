ad68a7ce249ffd657794f7bb0b47f08f8f3439c4
tomas :fuentes <tafuentesc@gmail.com>
2013-06-25 04:16:22 -0400

    Se agrega before_destroy a Sala de forma de eliminar las evaluaciones asociadas si se borra la sala

--- educaUC/app/models/sala.rb
+++ educaUC/app/models/sala.rb
@@ -2,6 +2,13 @@ class Sala < ActiveRecord::Base
   attr_accessible :centro_id, :nombre
   belongs_to :centro
   
+  # Lo anterior es equivalente a:
+  # has_many :evaluaciones, :dependent => :destroy
+  
+  # Si se borra, se deben destruir las evaluaciones asociadas
+  # debe ser before o no podremos saber cuáles tenemos que destruir
+  before_destroy :destroy_dependent_evaluaciones
+  
   # el nombre debe ser único para un centro dado!
   validates_uniqueness_of :nombre, :scope => [:centro_id]
   
@@ -23,6 +30,13 @@ class Sala < ActiveRecord::Base
   	# para cada evaluación en la lista, cambiamos su sala
   	evals.each do |eval|
   		eval.update_attribute(:sala, self)
+  	end
+  end
+  
+  # Método que destruye las evaluaciones asociadas
+  def destroy_dependent_evaluaciones
+  	self.evaluaciones.each do |eval|
+  		eval.destroy
   	end
   end
 end
